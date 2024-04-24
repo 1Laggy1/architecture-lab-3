4557d3e78188c0eef093a9d731ba0826a947e7f5
Strelia Illia <seylvin2005@gmail.com>
2024-04-24 15:01:30 +0300

    Implemented logic of drawing of figure - Implemented mouse handler

--- ui/window.go
+++ ui/window.go
@@ -1,142 +1,152 @@
 package ui
 
 import (
-	"image"
-	"image/color"
-	"log"
+    "image"
+    "image/color"
+    "log"
 
-	"golang.org/x/exp/shiny/driver"
-	"golang.org/x/exp/shiny/imageutil"
-	"golang.org/x/exp/shiny/screen"
-	"golang.org/x/image/draw"
-	"golang.org/x/mobile/event/key"
-	"golang.org/x/mobile/event/lifecycle"
-	"golang.org/x/mobile/event/mouse"
-	"golang.org/x/mobile/event/paint"
-	"golang.org/x/mobile/event/size"
+    "golang.org/x/exp/shiny/driver"
+    "golang.org/x/exp/shiny/screen"
+    "golang.org/x/image/draw"
+    "golang.org/x/mobile/event/key"
+    "golang.org/x/mobile/event/lifecycle"
+    "golang.org/x/mobile/event/mouse"
+    "golang.org/x/mobile/event/paint"
+    "golang.org/x/mobile/event/size"
 )
 
 type Visualizer struct {
-	Title         string
-	Debug         bool
-	OnScreenReady func(s screen.Screen)
+    Title         string
+    Debug         bool
+    OnScreenReady func(s screen.Screen)
 
-	w    screen.Window
-	tx   chan screen.Texture
-	done chan struct{}
+    w    screen.Window
+    tx   chan screen.Texture
+    done chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+    sz  size.Event
+    pos image.Rectangle
+
+    // Додаткові поля для відображення хрестика
+    crossX, crossY int
 }
 
-func (pw *Visualizer) Main() {
-	pw.tx = make(chan screen.Texture)
-	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
-	driver.Main(pw.run)
+func (pv *Visualizer) Main() {
+    pv.tx = make(chan screen.Texture)
+    pv.done = make(chan struct{})
+    pv.pos.Max.X = 800
+    pv.pos.Max.Y = 800
+    driver.Main(pv.run)
 }
 
-func (pw *Visualizer) Update(t screen.Texture) {
-	pw.tx <- t
+func (pv *Visualizer) Update(t screen.Texture) {
+    pv.tx <- t
 }
 
-func (pw *Visualizer) run(s screen.Screen) {
-	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
-	})
-	if err != nil {
-		log.Fatal("Failed to initialize the app window:", err)
-	}
-	defer func() {
-		w.Release()
-		close(pw.done)
-	}()
+func (pv *Visualizer) run(s screen.Screen) {
+    w, err := s.NewWindow(&screen.NewWindowOptions{
+        Title: pv.Title,
+				Width:  800,
+        Height: 800,
+    })
+    if err != nil {
+        log.Fatal("Failed to initialize the app window:", err)
+    }
+    defer func() {
+        w.Release()
+        close(pv.done)
+    }()
 
-	if pw.OnScreenReady != nil {
-		pw.OnScreenReady(s)
-	}
+    if pv.OnScreenReady != nil {
+        pv.OnScreenReady(s)
+    }
 
-	pw.w = w
+    pv.w = w
+		pv.crossX = 350
+    pv.crossY = 350
 
-	events := make(chan any)
-	go func() {
-		for {
-			e := w.NextEvent()
-			if pw.Debug {
-				log.Printf("new event: %v", e)
-			}
-			if detectTerminate(e) {
-				close(events)
-				break
-			}
-			events <- e
-		}
-	}()
+    events := make(chan interface{})
+    go func() {
+        for {
+            e := w.NextEvent()
+            if pv.Debug {
+                log.Printf("new event: %v", e)
+            }
+            if detectTerminate(e) {
+                close(events)
+                break
+            }
+            events <- e
+        }
+    }()
 
-	var t screen.Texture
+    var t screen.Texture
 
-	for {
-		select {
-		case e, ok := <-events:
-			if !ok {
-				return
-			}
-			pw.handleEvent(e, t)
+    for {
+        select {
+        case e, ok := <-events:
+            if !ok {
+                return
+            }
+            pv.handleEvent(e, t)
 
-		case t = <-pw.tx:
-			w.Send(paint.Event{})
-		}
-	}
+        case t = <-pv.tx:
+            w.Send(paint.Event{})
+        }
+    }
 }
 
-func detectTerminate(e any) bool {
-	switch e := e.(type) {
-	case lifecycle.Event:
-		if e.To == lifecycle.StageDead {
-			return true // Window destroy initiated.
-		}
-	case key.Event:
-		if e.Code == key.CodeEscape {
-			return true // Esc pressed.
-		}
-	}
-	return false
+func detectTerminate(e interface{}) bool {
+    switch e := e.(type) {
+    case lifecycle.Event:
+        if e.To == lifecycle.StageDead {
+            return true // Window destroy initiated.
+        }
+    case key.Event:
+        if e.Code == key.CodeEscape {
+            return true // Esc pressed.
+        }
+    }
+    return false
 }
 
-func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
-	switch e := e.(type) {
+func (pv *Visualizer) handleEvent(e interface{}, t screen.Texture) {
+    switch e := e.(type) {
+    case size.Event: // Оновлення даних про розмір вікна.
+        pv.sz = e
 
-	case size.Event: // Оновлення даних про розмір вікна.
-		pw.sz = e
+    case error:
+        log.Printf("ERROR: %s", e)
 
-	case error:
-		log.Printf("ERROR: %s", e)
+    case mouse.Event:
+        if t == nil {
+            if e.Button == mouse.ButtonRight {
+                // Конвертуємо координати миші до цілих чисел перед присвоєнням
+                pv.crossX, pv.crossY = int(e.X), int(e.Y)
+                pv.drawDefaultUI()
+            }
+        }
 
-	case mouse.Event:
-		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
-		}
-
-	case paint.Event:
-		// Малювання контенту вікна.
-		if t == nil {
-			pw.drawDefaultUI()
-		} else {
-			// Використання текстури отриманої через виклик Update.
-			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
-		}
-		pw.w.Publish()
-	}
+    case paint.Event:
+        if t == nil {
+            pv.drawDefaultUI()
+        } else {
+            pv.w.Scale(pv.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
+        }
+        pv.w.Publish()
+    }
 }
 
-func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+func (pv *Visualizer) drawDefaultUI() {
+    pv.w.Fill(pv.sz.Bounds(), color.RGBA{0, 255, 0, 255}, draw.Src) // Фон зеленого кольору.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+    // Малювання жовтого хрестика
+    crossX := pv.crossX - 50
+    crossY := pv.crossY - 50
+    pv.w.Fill(image.Rect(crossX, pv.crossY-5, crossX+100, pv.crossY+5), color.RGBA{255, 255, 0, 255}, draw.Src)
+    pv.w.Fill(image.Rect(pv.crossX-5, crossY, pv.crossX+5, crossY+100), color.RGBA{255, 255, 0, 255}, draw.Src)
 
-	// Малювання білої рамки.
-	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
-		pw.w.Fill(br, color.White, draw.Src)
-	}
+    // Виправлення координат хрестика
+    pv.crossX += 50
+    pv.crossY += 50
 }
